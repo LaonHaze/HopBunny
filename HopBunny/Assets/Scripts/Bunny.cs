e4af28db7c211a0dbc49e276ff40379ac7407783
Jae Ban <banjy0811@gmail.com>
2018-07-23 16:14:28 +0900

    Sprite Anim rb works

---
+++ HopBunny/Assets/Scripts/Bunny.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bunny : MonoBehaviour {
+    Animator bunnyanim;
+
+    void Start()
+    {
+        bunnyanim = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            bunnyanim.SetTrigger("touchDown");
+        }
+        
+    }
+}
