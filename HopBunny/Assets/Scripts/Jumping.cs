02000d29cfad45cf0124a1c573d12b8739a99e43
Jae Ban <banjy0811@gmail.com>
2018-07-23 18:34:03 +0900

    bunny bouncing!

---
+++ HopBunny/Assets/Scripts/Jumping.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour {
+
+    public float jumpForce = 6f;
+    Rigidbody2D rb;
+    Animator bunnyanim;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb = collision.collider.GetComponent<Rigidbody2D>();
+        bunnyanim = collision.collider.GetComponentInChildren<Animator>();
+
+        if (rb != null)
+        {
+            StartCoroutine(JumpAnim());
+        }
+    }
+
+    IEnumerator JumpAnim()
+    {
+        bunnyanim.SetTrigger("touchDown");
+        yield return new WaitForSeconds(seconds: 0.225f);
+        Vector2 velocity = rb.velocity;
+        velocity.y = jumpForce;
+        rb.velocity = velocity;
+    }
+}
